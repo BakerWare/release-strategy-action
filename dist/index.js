de4d15fcf1af398c0ff8bb4b8ee790d650ee24ba
Thijs van Drongelen <thijs.van.drongelen@bakerware.nl>
2022-11-18 14:50:13 +0100

    Dump jql result v2

--- dist/index.js
+++ dist/index.js
@@ -88497,7 +88497,7 @@ function run(tools) {
         console.log(result);
         if (result !== undefined && result.issues) {
             for (const issue of result.issues) {
-                console.log(issue.fields.issueType);
+                console.log(issue.fields);
             }
         }
         // haal issues op uit jira
